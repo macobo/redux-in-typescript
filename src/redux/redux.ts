0568728c9eb23dfebaca329d58a3a4bee0456cc3
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2019-02-24 17:06:56 +0200

    Subscription management in redux

--- src/redux/redux.ts
+++ src/redux/redux.ts
@@ -3,17 +3,34 @@ type Reducer<StateType, ActionType> = (state: StateType, action: ActionType) => 
 export class ReduxStore<StateType, ActionType> {
     private state: StateType
     private reducer: Reducer<StateType, ActionType>
+    private subscriptionIndex: number
+    private subscriptions: { [i: number]: () => void }
 
     constructor(reducer: Reducer<StateType, ActionType>, initialState: StateType) {
         this.state = initialState
         this.reducer = reducer
+        this.subscriptionIndex = 0
+        this.subscriptions = {}
     }
 
     dispatch(action: ActionType): void {
         this.state = this.reducer(this.state, action)
+
+        for (let key in this.subscriptions) {
+            this.subscriptions[key]()
+        }
     }
 
     getState(): StateType {
         return this.state
+    }
+
+    subscribe(fn: () => void): number {
+        this.subscriptions[this.subscriptionIndex] = fn
+        return this.subscriptionIndex++
+    }
+
+    unsubscribe(index: number) {
+        delete this.subscriptions[index]
     }
 }
